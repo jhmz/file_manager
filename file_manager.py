841b73d0fb4617becd2e0637b00ce9d335b15570
jhmz <joaohenriquemuniz@gmail.com>
2021-09-11 21:23:58 -0300

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -2,39 +2,33 @@ import os
 from shutil import move
 #path='F:\\example.txt'; extension = '.exe'; name = 'test'
 def rename_files(path, extension, name):
-    try:
-        folder = os.listdir(path)
-        count = 0
+    folder = os.listdir(path)
+    count = 0
 
-        for i in folder:
-            if(extension in i):
-                old = path + '\\{}'.format(i)
-                new = path + '\\{}{}{}'.format(name,count,extension)
-                os.rename(old,new)
-                count = count + 1
-
-    except:
-        print('Invalid path or extension')
+    for i in folder:
+        if(extension in i):
+            old = path + '\\{}'.format(i)
+            new = path + '\\{}{}{}'.format(name,count,extension)
+            os.rename(old,new)
+            count = count + 1
 
 #function that organizes the files in folders separated by extension
 def organize_extensions(path):
-    try:
-        folder = os.listdir(path)
-        files = []
-        extensions = []
-        #getting files and extensions path
-        for file in folder:
-            filename, extension = os.path.splitext(file)
-            files.append(file)
-            if(extension not in extensions):
-                extensions.append(extension)
+    folder = os.listdir(path)
+    files = []
+    extensions = []
+    #getting files and extensions path
+    for file in folder:
+        filename, extension = os.path.splitext(file)
+        files.append(file)
+        if(extension.lower() not in extensions):
+            extensions.append(extension.lower())
 
-        for i in range(len(extensions)):
-            if(extensions[i] == 'ini'):
-                extensions.pop(i)
-            elif(extensions[i]== ''):
-                extensions.pop(i)
-
+    for i in range(len(extensions)):
+        if(extensions[i] == 'ini'):
+            extensions.pop(i)
+        elif(extensions[i]== ''):
+            extensions.pop(i)
             #creating folders for extensions
         for extension in extensions:
             os.mkdir(path+'\\'+extension)
@@ -42,7 +36,4 @@ def organize_extensions(path):
         for file in files:
             for extension in extensions:
                 if(extension in file):
-                    move(path+'\\'+file,path+'\\'+extension+'\\'+file)
-                    
-    except:
-        print('Invalid path or extension')
+                    move(path+'\\'+file,path+'\\'+extension.lower()+'\\'+file)
